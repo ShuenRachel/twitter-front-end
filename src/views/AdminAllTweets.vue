7f251c9fc3ddc6753676b13f8198806226ecf485
Jater <p833795p@gmail.com>
2022-02-23 23:39:20 +0800

    feat: build adminTweets component.

--- src/views/AdminAllTweets.vue
+++ src/views/AdminAllTweets.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>AdminAllTweets Page</h1>
+  <div>
+    <AdminTweets />
+  </div>
 </template>
+
+<script>
+import AdminTweets from '../component/AdminTweets.vue'
+
+export default {
+  components: {
+    AdminTweets,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/main.scss";
+  div.fake-nav {
+    min-width: 378px;
+    border-right: 1px solid $border;
+  }
+</style>
