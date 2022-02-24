39a6c2870a7ff6535fe6ddbff99153eadfc79cc7
rachellam <rachellam@tuofaninfo.com>
2022-02-24 23:28:19 +0800

    style: navbar layout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,15 +1,77 @@
 <template>
   <div class="navbar-panel">
-    this is navbar
+    <div class="nav-wrapper">
+      <div class="nav-icon"><img src="/image/logo.svg" alt="" srcset="" /></div>
+      <div class="nav-content" v-if="isAdmin">
+        <div class="link-wrapper">
+          <router-link class="link" to="/admin/tweets">推文清單</router-link>
+          <router-link class="link" to="/admin/tweets">使用者列表</router-link>
+        </div>
+      </div>
+      <div class="nav-content" v-else>
+        <div class="link-wrapper">
+          <router-link class="link" to="/user/home">首頁</router-link>
+          <router-link class="link" to="/user/home">個人資料</router-link>
+          <router-link class="link" to="/user/setting">設定</router-link>
+          <button class="btn btn-orange">推文</button>
+        </div>
+      </div>
+      <div class="nav-footer">登出</div>
+    </div>
   </div>
 </template>
+
+<script>
+// import { defineComponent } from '@vue/composition-api'
+
+export default {
+  data() {
+    return {
+      isAdmin: false,
+    };
+  },
+};
+</script>
 
 <style lang="scss" scoped>
 @import "../assets/scss/main.scss";
 .navbar-panel {
   border-right: 1px solid $card-fill;
-  background: red;
-  // width: 379px;
-  // height: 100vh;
+  .nav-wrapper {
+    margin-left: 113px;
+    margin-right: 55px;
+    height: 100%;
+    display: flex;
+    flex-flow: column nowrap;
+    justify-content: space-between;
+  }
+}
+
+.nav {
+  &-icon {
+    width: 30px;
+    height: 30px;
+    margin-top: 14px;
+    margin-bottom: 45px;
+  }
+  &-content {
+    display: flex;
+    flex-flow: column nowrap;
+    flex-grow: 1;
+    .link-wrapper {
+      display: flex;
+      flex-flow: column nowrap;
+      .link {
+        font-weight: 700;
+        font-size: 18px;
+        color: $text-main;
+        &:hover {
+          color: $brand-orange;
+        }
+      }
+    }
+  }
+  &-footer {
+  }
 }
 </style>
