51419e782cdbd876682254b321f8744e8cc7e3aa
Jater <p833795p@gmail.com>
2022-02-24 16:18:41 +0800

    feat: build AdminUsers component.

--- src/views/AdminAllUsers.vue
+++ src/views/AdminAllUsers.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>AdminAllUsers Page</h1>
+  <div class="admin">
+    <section class="users-card">
+      <AdminUsers />
+    </section>
+  </div>
 </template>
+
+<script>
+import AdminUsers from '../component/AdminUsers.vue'
+
+export default {
+  components: {
+    AdminUsers,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  @import "../assets/scss/main.scss";
+
+  section.users-card {
+    padding: 15px;
+  }
+</style>
