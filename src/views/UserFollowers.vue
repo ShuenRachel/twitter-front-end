f01296168c58f61d6a1124526d230accb53c6bf3
Jater <p833795p@gmail.com>
2022-02-26 19:22:18 +0800

    feat: build NavTabs and FollowList.

--- src/views/UserFollowers.vue
+++ src/views/UserFollowers.vue
@@ -1,3 +1,28 @@
 <template>
-  <h1>UserFollowers Page.</h1>
+  <div class="user-follower-container">
+    <section class="follow-nav-tab">
+      <NavTabs />
+    </section>
+    <section class="follow-list">
+      <FollowList />
+    </section>
+  </div>
 </template>
+
+<script>
+// import authorizationAPI from './../apis/authorization'
+import NavTabs from "../components/NavTabs";
+import FollowList from "../components/FollowList.vue";
+
+export default {
+  components: {
+    NavTabs,
+    FollowList,
+  },
+  data() {
+    return {
+      followers: [],
+    };
+  },
+};
+</script>
