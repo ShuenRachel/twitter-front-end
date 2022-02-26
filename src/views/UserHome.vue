37d5a2470f547d63e3584ab12f0291c35f8372ba
rachellam <rachellam@tuofaninfo.com>
2022-02-26 23:37:00 +0800

    feat: user home page set up api config

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>UserHome Page.</h1>
+  <div>
+    <TweetsList :init-tweets-data="tweetsData" />
+  </div>
 </template>
+
+<script>
+import TweetsList from "@/components/TweetsList.vue"
+// import tweetsAPI from "./../apis/tweets"
+export default {
+  components: {
+    TweetsList
+  },
+  data() {
+    return {
+      tweetsData: []
+    }
+  },
+  methods: {
+    // async fetchTweets() {
+    //   try {
+    //     const response = await tweetsAPI
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // }
+  },
+  created() {
+    // this.fetchTweets()
+  }
+}
+</script>
