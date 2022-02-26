e7b71c1a7269dcec0ef781a090793073d825a3b8
rachellam <rachellam@tuofaninfo.com>
2022-02-27 00:07:20 +0800

    feat: add vue placeholder in tweetlis

--- src/components/TweetsList.vue
+++ src/components/TweetsList.vue
@@ -1,17 +1,18 @@
 <template>
   <div class="tweet-container">
     <div class="tweet">
+      <!-- TODO: import user avatar -->
       <div class="tweet__user-avatar" style="background-image: url('https://via.placeholder.com/50x50/DFDFDF?text=No+Image');">
       </div>
       <div class="tweet__info-container">
         <div class="info">
-          <span class="name">Apple</span>
-          <span class="account">apple・3 小時</span>
+          <span class="name">{{tweet.tweetUserName}}</span>
+          <span class="account">apple・{{tweet.createdAt | fromNow}}</span>
           <div v-if="isReply" class="reply-account">
             回覆 <span class="reply-account">daniel</span>
           </div>
           <div class="content">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing ...</p>
+            <p>{{tweet.description}}</p>
           </div>
         </div>
         <div v-if="isUserPage" class="tweet__footer">
@@ -20,7 +21,7 @@
                 <svg class="actives-icon actives-icon__reply" width="12" height="12" viewBox="0 0 21 20" fill="#657786" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12.8461 0.241934L8.69812 0.231934H8.69612C4.32212 0.231934 0.896118 3.65893 0.896118 8.03393C0.896118 12.1319 4.08212 15.2399 8.36112 15.4039V19.2319C8.36112 19.3399 8.40512 19.5179 8.48112 19.6349C8.62312 19.8599 8.86512 19.9819 9.11312 19.9819C9.25112 19.9819 9.39012 19.9439 9.51512 19.8639C9.77912 19.6959 15.9881 15.7239 17.6031 14.3579C19.5051 12.7479 20.6431 10.3879 20.6461 8.04593V8.02893C20.6401 3.66193 17.2161 0.241933 12.8461 0.240933V0.241934ZM16.6331 13.2139C15.4991 14.1739 11.7711 16.6189 9.86112 17.8569V14.6699C9.86112 14.2559 9.52612 13.9199 9.11112 13.9199H8.71512C5.05512 13.9199 2.39712 11.4439 2.39712 8.03393C2.39712 4.49993 5.16512 1.73193 8.69712 1.73193L12.8441 1.74193H12.8461C16.3781 1.74193 19.1461 4.50793 19.1481 8.03793C19.1451 9.94793 18.2061 11.8819 16.6341 13.2139H16.6331Z" />
                 </svg>
-                <span class="tweet__footer__actives__count">13</span>
+                <span class="tweet__footer__actives__count">{{tweet.repliedCount}}</span>
               </div>
               <div @click="handleLike" class="tweet__footer__actives__like">
                 <svg v-if="!isLike" class="actives-icon actives-icon__unlike" width="12.5" height="11.8" viewBox="0 0 21 20" fill="#657786" xmlns="http://www.w3.org/2000/svg">
@@ -29,7 +30,7 @@
                 <svg v-if="isLike" class="actives-icon actives-icon__like" width="20" height="18" viewBox="0 0 22 20" fill="#E0245E" xmlns="http://www.w3.org/2000/svg">
                 <path d="M11 19.6381H10.986C8.40295 19.5901 0.949951 12.8561 0.949951 6.47812C0.949951 3.41412 3.47495 0.724121 6.35295 0.724121C8.64295 0.724121 10.183 2.30412 10.999 3.45412C11.813 2.30612 13.353 0.724121 15.644 0.724121C18.524 0.724121 21.048 3.41412 21.048 6.47912C21.048 12.8551 13.594 19.5891 11.011 19.6361H11V19.6381Z" />
                 </svg>
-                <span class="tweet__footer__actives__count" :class="{ liked: isLike }">76</span>
+                <span class="tweet__footer__actives__count" :class="{ liked: isLike }">{{tweet.likeCount}}</span>
               </div>
           </div>
         </div>
@@ -42,6 +43,7 @@
 </template>
 
 <script>
+import { fromNowFilter } from "../utils/mixin";
 export default {
   props:{
     initTweetData: {
@@ -51,7 +53,7 @@ export default {
   },
   data() {
     return {
-      tweetData: {},
+      tweet: {},
       // TODO: import vuex control components
       isUserPage: true,
       isLike: false,
@@ -60,7 +62,7 @@ export default {
   },
   methods: {
     fetchTweet() {
-      this.tweetData = this.initTweetData
+      this.tweet = this.initTweetData
     },
     handleLike() {
       this.isLike = !this.isLike;
@@ -68,7 +70,8 @@ export default {
   },
   created() {
     this.fetchTweet()
-  }
+  },
+  mixins: [fromNowFilter],
 }
 </script>
 
