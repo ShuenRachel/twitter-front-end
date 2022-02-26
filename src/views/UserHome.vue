f299d2433c2d5151c00122416cb49d5b639ebd7e
rachellam <rachellam@tuofaninfo.com>
2022-02-26 23:24:43 +0800

    feat: import new tweet to user home page

--- src/views/UserHome.vue
+++ src/views/UserHome.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>UserHome Page.</h1>
+  <div>
+    <TweetNew class="tweet-new" />
+  </div>
 </template>
+
+<script>
+import TweetNew from "@/components/TweetNew.vue";
+
+export default {
+  components: {
+    TweetNew,
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/main.scss";
+.tweet-new {
+  border-top: $border-setting;
+  border-bottom: 10px solid $border-color;
+}
+</style>
