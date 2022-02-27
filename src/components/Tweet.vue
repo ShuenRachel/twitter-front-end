d2798964f0a5128423d587a4e360dc207dccb92c
rachellam <rachellam@tuofaninfo.com>
2022-02-27 15:26:26 +0800

    feat: built html

---
+++ src/components/Tweet.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="tweet-wrapper">
+    <div class="tweet-content">
+      <div class="tweet-content-header">
+        <div class="user-avatar"></div>
+        <div class="user-data">
+          <span class="user-data-name">name</span>
+          <span class="user-data-account">account</span>
+        </div>
+      </div>
+      <div class="tweet-content">
+        <p class="tweet-content-text"></p>
+        <span class="tweet-content-time">2020/10/22</span>
+      </div>
+    </div>
+    <div class="tweet-data">
+      <span class="tweet-data-replies">3 Replies</span>
+      <span class="tweet-data-likes">10 Likes</span>
+    </div>
+    <div class="tweet-action">
+      <div class="btn-like"></div>
+      <div class="btn-comment"></div>
+    </div>
+  </div>
+</template>
--- src/views/UserTweet.vue
+++ src/views/UserTweet.vue
@@ -1,3 +1,6 @@
 <template>
-  <h1>UserTweet Page.</h1>
+  <div>
+
+  </div>
 </template>
+
