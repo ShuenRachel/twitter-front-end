8d0552054d16a537bd82ad419882b1ec0eaa0df8
rachellam <rachellam@tuofaninfo.com>
2022-03-02 23:03:48 +0800

    fix: navbar active class

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="page-container">
-    <Navbar class="container-left" @after-show-modal="afterShowModal"/>
+    <Navbar class="container-left" :is-current-user-page="isCurrentUserPage" @after-show-modal="afterShowModal"/>
     <div class="container-middle"><Header /><router-view class="container-middle-main" /></div>
     <PopularUsers class="container-right" v-if="!isAdmin" />
     <TweetModal v-if="modalVisibility" @after-close-modal="afterCloseModal" />
@@ -23,11 +23,12 @@ export default {
   },
   data() {
     return {
-      modalVisibility: false
+      modalVisibility: false,
+      isCurrentUserPage: false
     }
   },
   computed: {
-    ...mapState(["isAdmin"])
+    ...mapState(["isAdmin", "currentUser"])
   },
   methods: {
     afterShowModal() {
@@ -36,6 +37,21 @@ export default {
     afterCloseModal() {
       this.modalVisibility = false
     }
+  },
+  beforeRouteUpdate(to, from, next) {
+    console.log('this.isCurrentUserPage', this.isCurrentUserPage)
+    console.log('Number(to.params.user_id)', Number(to.params.user_id))
+    console.log('t/f', Number(to.params.user_id) === this.currentUser.id)
+    this.isCurrentUserPage = Number(to.params.user_id) === this.currentUser.id
+    console.log('this.isCurrentUserPage 2', this.isCurrentUserPage)
+    // console.log('userID:' , to.params.user_id)
+    // console.log('true false:' , Number(to.params.user_id) === this.currentUser.id)
+    // if (Number(to.params.user_id) === this.currentUser.id) {
+    //   this.isCurrentUserPage = Number(to.params.user_id) === this.currentUser.id
+    // } else {
+    //   this.isCurrentUserPage = false
+    // }
+    next()
   }
 };
 </script>
